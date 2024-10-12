96cf4c1d8036f202b987b1ad5898a7c82c1f2922
Rath Rene <rath@oxl.at>
2024-10-12 17:32:41 +0200

    update nftables exclude-regex

--- report_script/log_watcher_nftables.sh
+++ report_script/log_watcher_nftables.sh
@@ -13,7 +13,7 @@ fi
 LOG_FILE="$1"
 
 TOKEN=''  # optional supply an API token
-EXCLUDE_REGEX='ALLOW|output|=lo|=192.168.|=172.16.|=10.|=127.'
+EXCLUDE_REGEX='ALLOW|output|=lo|=192.168.|=172.16.|=172.17.|=172.18.|=172.19.|=172.20.|=172.21.|=172.22.|=172.23.|=172.24.|=172.25.|=172.26.|=172.27.|=172.28.|=172.29.|=172.30.|=172.31.|=10.|=127.'
 MAX_PARALLEL=10
 
 # NOTE: Bash regex does not support PCRE like '\d' '\s' nor non-greedy '*?'
