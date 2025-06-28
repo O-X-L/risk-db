30e2f4e2ba16c977107d2bb274a8a831903066b9
Rath Pascal <rath@oxl.at>
2025-06-28 15:24:55 +0200

    updated dynamic asn hosting-kind check

--- src/riskdb/builder/obj/asn.py
+++ src/riskdb/builder/obj/asn.py
@@ -6,7 +6,7 @@ from riskdb.builder.obj.report import Report
 
 ASN_KINDS = ['hosting', 'vpn', 'scanner', 'crawler', 'isp', 'education']
 ASN_FIND = {
-    'hosting': ['host', 'cloud', 'server', 'datacenter', 'data center'],
+    'hosting': ['host', 'cloud', 'server', 'datacenter', 'data center', 'rack'],
     'isp': ['tel', 'mobil'],
     'education': [
         'university', 'universitat', 'universitaris', 'universitaet', 'universities',
